91744477964cd9d87c761acd1b5d73f2e7cc5960
Murat Byazrov <murat.byazrov@yandex.ru>
2020-05-21 11:39:51 +0300

    Commit after review. corrected: 1. the status error 2. request with incorrect id 3. processing connecting to mongoose

--- controllers/users.js
+++ controllers/users.js
@@ -7,17 +7,18 @@ module.exports.getUsers = (req, res) => {
     .catch((err) => res.status(500).send({ message: err.message }));
 };
 
-module.exports.getUserById = (req, res) => {
-  User.findById(req.params.id)
-    .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).sens({ message: err.message }));
-};
 
+module.exports.getUserById = async (req, res) => {
+  await User.findById(req.params.id)
+    .orFail(new Error('Нет такого пользователя'))
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(404).send({ message: err.message }));
+};
 
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
 
   User.create({ name, about, avatar })
     .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch((err) => res.status(400).send({ message: err.message }));
 };
